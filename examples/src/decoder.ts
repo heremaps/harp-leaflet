eb693e50da1b3be3cfd804911bba521b750d6ca4
Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>
2019-11-29 10:03:20 +0100

    Update harp.gl packages to 0.11.1.
    Signed-off-by: Zbigniew Zagorski <ext-zbyszek.zagorski@here.com>

--- examples/src/decoder.ts
+++ examples/src/decoder.ts
@@ -10,10 +10,7 @@ declare let self: Worker & {
 
 self.importScripts("https://unpkg.com/three@0.110.0/build/three.min.js");
 
-import { GeoJsonTileDecoderService } from "@here/harp-geojson-datasource/index-worker";
 import { OmvTileDecoderService, OmvTilerService } from "@here/harp-omv-datasource/index-worker";
 
 OmvTileDecoderService.start();
-OmvTilerService.start(); // TODO: See if this can be separated to get 2 scripts of smaller sizes.
-// TODO: after adding the features datasource, test if this can be removed, normally yes.
-GeoJsonTileDecoderService.start();
+OmvTilerService.start();
